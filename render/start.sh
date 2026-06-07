28ed92e709d760cfb9901e533eb5c081699dd589
unknown <syafrizalcestanorm@gmail.com>
2026-06-07 13:55:47 +0700

    update start.sh

--- render/start.sh
+++ render/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+mkdir -p /tmp/views
 php artisan migrate --force
 php artisan db:seed --force
 php artisan storage:link --force
 php artisan view:clear
 php artisan cache:clear
 php artisan config:clear
-chmod -R 775 /var/www/storage
-chmod -R 775 /var/www/bootstrap/cache
+chmod -R 777 /var/www/storage
+chmod -R 777 /var/www/bootstrap/cache
 php-fpm -D
 nginx -g "daemon off;"
