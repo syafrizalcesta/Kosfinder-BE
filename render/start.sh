c606ad2347acd169d63dd50061dc0304459c104f
syafrizalcesta <syafrizalcestanorm@gmail.com>
2026-06-07 13:28:28 +0700

    Update start.sh

--- render/start.sh
+++ render/start.sh
@@ -2,6 +2,9 @@
 php artisan migrate --force
 php artisan db:seed --force
 php artisan storage:link --force
+php artisan view:clear
+php artisan cache:clear
+php artisan config:clear
 chmod -R 775 /var/www/storage
 chmod -R 775 /var/www/bootstrap/cache
 php-fpm -D
