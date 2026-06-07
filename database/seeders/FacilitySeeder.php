d1ff1c696e3112dc0c31630bc2331bbc02db7be2
unknown <syafrizalcestanorm@gmail.com>
2026-06-07 19:31:52 +0700

    fix seeder rule n facil V2

--- database/seeders/FacilitySeeder.php
+++ database/seeders/FacilitySeeder.php
@@ -11,17 +11,17 @@ class FacilitySeeder extends Seeder
     public function run(): void
     {
         $facilities = [
-            ['id' => 'FAC-WIFI01', 'name' => 'WiFi', 'category' => 'Konektivitas'],
-            ['id' => 'FAC-AC0001', 'name' => 'AC', 'category' => 'Kamar'],
-            ['id' => 'FAC-KMI001', 'name' => 'Kamar Mandi Dalam', 'category' => 'Kamar'],
-            ['id' => 'FAC-PKM001', 'name' => 'Parkir Motor', 'category' => 'Fasilitas Luar'],
-            ['id' => 'FAC-PKM002', 'name' => 'Parkir Mobil', 'category' => 'Fasilitas Luar'],
-            ['id' => 'FAC-DPR001', 'name' => 'Dapur Umum', 'category' => 'Fasilitas Bersama'],
-            ['id' => 'FAC-LDR001', 'name' => 'Laundry', 'category' => 'Layanan'],
-            ['id' => 'FAC-CTV001', 'name' => 'CCTV', 'category' => 'Keamanan'],
-            ['id' => 'FAC-SEC001', 'name' => 'Keamanan 24 Jam', 'category' => 'Keamanan'],
-            ['id' => 'FAC-LST001', 'name' => 'Include Listrik', 'category' => 'Biaya'],
-            ['id' => 'FAC-FAN001', 'name' => 'Kipas', 'category' => 'Kamar'],
+            ['id' => 'FAC-WIFI01', 'name' => 'WiFi', 'category' => 'Konektivitas', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835222/icon-wifi_g2fvch.svg'],
+            ['id' => 'FAC-AC0001', 'name' => 'AC', 'category' => 'Kamar', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835218/icon-ac_ggigec.svg'],
+            ['id' => 'FAC-KMI001', 'name' => 'Kamar Mandi Dalam', 'category' => 'Kamar', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835218/icon-km-dalam_pxlr48.svg'],
+            ['id' => 'FAC-PKM001', 'name' => 'Parkir Motor', 'category' => 'Fasilitas Luar', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835221/icon-parkir_fud8aj.svg'],
+            ['id' => 'FAC-PKM002', 'name' => 'Parkir Mobil', 'category' => 'Fasilitas Luar', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835221/icon-parkir-mbl_vuot9i.svg'],
+            ['id' => 'FAC-DPR001', 'name' => 'Dapur Umum', 'category' => 'Fasilitas Bersama', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835218/icon-dapur_ingowm.svg'],
+            ['id' => 'FAC-LDR001', 'name' => 'Laundry', 'category' => 'Layanan', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835218/icon-laundry_e7rbkg.svg'],
+            ['id' => 'FAC-CTV001', 'name' => 'CCTV', 'category' => 'Keamanan', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835218/icon-cctv_wk9jld.svg'],
+            ['id' => 'FAC-SEC001', 'name' => 'Keamanan 24 Jam', 'category' => 'Keamanan', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835222/icon-security_mih4k4.svg'],
+            ['id' => 'FAC-LST001', 'name' => 'Include Listrik', 'category' => 'Biaya', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835220/icon-listrik_ouzvt0.svg'],
+            ['id' => 'FAC-FAN001', 'name' => 'Kipas', 'category' => 'Kamar', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835220/icon-kipas_s48dci.svg'],
         ];
 
         foreach ($facilities as $fac) {
@@ -30,7 +30,7 @@ class FacilitySeeder extends Seeder
                 [
                     'facility_name' => $fac['name'],
                     'category' => $fac['category'],
-                    'icon_url' => 'default-icon.svg',
+                    'icon_url' => $fac['icon'],
                     'created_at' => now(),
                     'updated_at' => now(),
                 ]
--- database/seeders/RuleSeeder.php
+++ database/seeders/RuleSeeder.php
@@ -11,11 +11,11 @@ class RuleSeeder extends Seeder
     public function run(): void
     {
         $rules = [
-            ['id' => 'RUL-SMK001', 'name' => 'Dilarang merokok', 'category' => 'Ketertiban'],
-            ['id' => 'RUL-JAM001', 'name' => 'Terdapat jam malam', 'category' => 'Akses'],
-            ['id' => 'RUL-LJN001', 'name' => 'Dilarang membawa lawan jenis', 'category' => 'Norma'],
-            ['id' => 'RUL-PET001', 'name' => 'Dilarang membawa peliharaan', 'category' => 'Kebersihan'],
-            ['id' => 'RUL-TAM001', 'name' => 'Tamu dilarang menginap', 'category' => 'Ketertiban'],
+            ['id' => 'RUL-SMK001', 'name' => 'Dilarang merokok', 'category' => 'Ketertiban', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835220/icon-no-smoking_sk6v4h.svg'],
+            ['id' => 'RUL-JAM001', 'name' => 'Terdapat jam malam', 'category' => 'Akses', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835218/icon-jam-malam_tna7g0.svg'],
+            ['id' => 'RUL-LJN001', 'name' => 'Dilarang membawa lawan jenis', 'category' => 'Norma', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835220/icon-no-guests_cphpq4.svg'],
+            ['id' => 'RUL-PET001', 'name' => 'Dilarang membawa peliharaan', 'category' => 'Kebersihan', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835220/icon-no-pets_mffhl2.svg'],
+            ['id' => 'RUL-TAM001', 'name' => 'Tamu dilarang menginap', 'category' => 'Ketertiban', 'icon' => 'https://res.cloudinary.com/djncmuz1d/image/upload/v1780835220/icon-no-stay_s6mbht.svg'],
         ];
 
         foreach ($rules as $rule) {
@@ -24,7 +24,7 @@ class RuleSeeder extends Seeder
                 [
                     'rule_name' => $rule['name'],
                     'category' => $rule['category'],
-                    'icon_url' => 'default-rule.svg',
+                    'icon_url' => $rule['icon'],
                     'created_at' => now(),
                     'updated_at' => now(),
                 ]
